803761ef913d6696ceff14b63c81d62bafd90d36
Rocio Sanchez <20101172@mail.wit.ie>
2022-11-21 19:13:48 +0000

    added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,13 +2,16 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
 
+
 app.use('/api/movies', moviesRouter);
+app.use(express.json());
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
