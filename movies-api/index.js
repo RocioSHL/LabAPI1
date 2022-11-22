50cdd0e9367125fa4ee10c1bf35f709222515232
Rocio Sanchez <20101172@mail.wit.ie>
2022-11-22 17:31:23 +0000

    Mongo lab Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genrerouter from './api/genres';
+import './db';
+import './seedData'
+
 
 
 dotenv.config();
