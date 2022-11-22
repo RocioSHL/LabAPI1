8ff322dcbcd5e03e0a34ab0b5e1b590fc4805f32
Rocio Sanchez <20101172@mail.wit.ie>
2022-11-22 18:36:49 +0000

    Mongo lab Error Handling

--- movies-api/index.js
+++ movies-api/index.js
@@ -8,12 +8,19 @@ import usersRouter from './api/users';
 
 
 dotenv.config();
-
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
 const app = express();
 
 const port = process.env.PORT;
 
-
+app.use(errHandler);
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres',genrerouter);
 app.use(express.json());
