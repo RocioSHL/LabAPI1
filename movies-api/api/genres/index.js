149ec5da9ded6d16ca9cbb1b31903ae62d6ef8a4
Rocio Sanchez <20101172@mail.wit.ie>
2022-11-22 19:00:49 +0000

    Mongo lab Exercises

--- movies-api/api/genres/index.js
+++ movies-api/api/genres/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { genres} from './genreData';
+import genre from './genresModel'
 import uniqid from 'uniqid'
 const router = express.Router(); 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
     res.json(genres);
-});
+});*/
 // Get movie genre
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
     const id = parseInt(req.params.id);
+   
     if (genres.id == id) {
         res.status(200).json(movieDetails);
     } else {
@@ -16,5 +18,9 @@ router.get('/:id', (req, res) => {
             status_code: 404
         });
     }
+});
+router.get('/', async (req, res) => {
+    const genres = await genre.find();
+    res.status(200).json(genres);
 });
 export default router;
